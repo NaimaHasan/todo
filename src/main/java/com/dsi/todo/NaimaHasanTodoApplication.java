1207b167396b2ecd9b0b4545fa1e43a9c7e0897a
NaimaHasan <naimahasan31@gmail.com>
2024-04-04 16:32:42 +0600

    initial commit

---
+++ src/main/java/com/dsi/todo/NaimaHasanTodoApplication.java
@@ -0,0 +1,13 @@
+package com.dsi.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaimaHasanTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaimaHasanTodoApplication.class, args);
+	}
+
+}
