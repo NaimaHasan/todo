2205e33d47d4d2a53d20490c6db8b3fe339d292c
NaimaHasan <naimahasan31@gmail.com>
2024-04-15 16:15:11 +0600

    changed some padding

--- src/main/resources/db/changelog/changes/001-initial-schema.sql
+++ src/main/resources/db/changelog/changes/001-initial-schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS todos (
     id  PRIMARY KEY,
     title TEXT,
     description TEXT,
-     priority TEXT;
-     status TEXT;
-     createdAt TEXT;
-     updatedAt TEXT;
+    priority TEXT;
+    status TEXT;
+    createdAt TEXT;
+    updatedAt TEXT;
 );
